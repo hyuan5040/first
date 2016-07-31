6a6e3ac7d255994e85bc27318de4f646a0025f38
hyuan71 <yuan_henry@yahoo.com>
2016-07-31 12:30:25 -0700

    1

--- Client/src/client/FindClosest.java
+++ Client/src/client/FindClosest.java
@@ -9,15 +9,24 @@ public class FindClosest {
     
     int findClosest(int [] nums, int target)  // 
     {
-        int n = findClosestGap(nums,target);
-        return target -n;
+        m_closestGap = Integer.MAX_VALUE;
+        findClosestGap(nums,target);
+        return target -m_closestGap;
     }
 
-    int findClosestGap(int [] nums, int target)  // 
+    void findClosestGap(int [] nums, int target)  // 11,5,9 : 23
     // as many times
     // 0 times
     // all positive
+    //   11,5,9 : 23
+    //   11,5,9 : 12
+    //   11,5,9 : 7   
     {
+        if(target <0)
+            return;
+        if(m_closestGap>target)
+            m_closestGap = target;
+/*            
       int min = Integer.MAX_VALUE;   ///
       for(int i=0;i<nums.length;i++)   // 10,8
       {
@@ -26,18 +35,19 @@ public class FindClosest {
       }
       if(target< min)
           return -1;
-      
+*/
       //int closest= Integer.MAX_VALUE;
-      for(int i=0;i<nums.length;i++)    // 11
+      for(int i=0;i<nums.length;i++)    // 1
       {
-          
-          int t2 = target - nums[i];     // 1
+          /*
+          int t2 = target - nums[i];     // 7
           if(t2> 0 && t2<m_closestGap)
               m_closestGap = t2;
-          
+          */
           //m_closestGap = min(m_closestGap, t2);   // 1
           
-          int closest2 = findClosest(nums,t2);   // [11], 1
+          findClosestGap(nums,target-nums[i]);   // [11,5,9], 12
+/*          
            if(closest2 <0)
           {
               continue;
@@ -48,9 +58,9 @@ public class FindClosest {
           {
               m_closestGap = closest2;
           }
-         
+*/         
       }
-      return m_closestGap;
+      //return m_closestGap;
     }
     
     public static void main(String [] args) {
